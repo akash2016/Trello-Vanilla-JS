028bba8587358f62793e7832f34d85e59a6148f4
akash2016 <akash.verma@evivehealth.com>
2017-12-23 17:57:35 +0530

    express server set up

---
+++ Express.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import open from 'open';
+import path from 'path';
+import webpack from 'webpack';
+import config from './webpack.config';
+
+const port = 3000;
+const app = express();
+
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
