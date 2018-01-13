cfcca5d60724eb19ec8a1fd220e5788b46a9a0d8
akash2016 <akash.verma@evivehealth.com>
2018-01-13 20:23:02 +0530

    Added other lanes in ui

--- src/view.js
+++ src/view.js
@@ -16,12 +16,39 @@ const createBoard = (cards) => {
     <div id="TODO">
    <h2> ${lane.TODO}</h2>
     ${cards.filter(card => (card.lane === lane.TODO)).map(card => `<div id=${card.id} class='card'>
-    ${card.name}
+    ${card.name}:
     ${card.description}
     <button class='move'>Hold</button>
     <button class='move'>In Progress</button>
     <button class='move'>Complete</button>
     </div>`)}
+    </div>
+    <div id="InProgress">
+    <h2> ${lane.INPROGRESS}</h2>
+    ${cards.filter(card => (card.lane === lane.INPROGRESS)).length > 0 ? cards.filter(card => (card.lane === lane.INPROGRESS)).map(card => `<div id=${card.id} class='card'>
+    ${card.name}:
+    ${card.description}
+    <button class='move'>Hold</button>
+    <button class='move'>Complete</button>
+    </div>`) : 'Nothing in Progress yet'}
+    </div>
+    <div id="Hold">
+    <h2> ${lane.HOLD}</h2>
+    ${cards.filter(card => (card.lane === lane.HOLD)).length > 0 ? cards.filter(card => (card.lane === lane.HOLD)).map(card => `<div id=${card.id} class='card'>
+    ${card.name}:
+    ${card.description}
+    <button class='move'>In Progress</button>
+    <button class='move'>Complete</button>
+    </div>`) : 'Nothing on hold yet'}
+    </div>
+    <div id="Complete">
+    <h2> ${lane.COMPLETE}</h2>
+    ${cards.filter(card => (card.lane === lane.COMPLETE)).length > 0 ? cards.filter(card => (card.lane === lane.COMPLETE)).map(card => `<div id=${card.id} class='card'>
+    ${card.name}:
+    ${card.description}
+    <button class='move'>In Progress</button>
+    <button class='move'>Hold</button>
+    </div>`) : 'Nothing complete yet'}
     </div>
     </div>`;
   }
